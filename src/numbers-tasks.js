789b097a8cfb2d2495d973eea45a459d72c7c86d
Oleksandr Murga <sanay8myrga@gmail.com>
2024-11-12 00:36:47 +0200

    feat:выполнил еще одну функци

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -109,8 +109,11 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dotProduct = x1 * x2 + y1 * y2;
+  const magnitudeA = Math.sqrt(x1 * x1 + y1 * y1);
+  const magnitudeB = Math.sqrt(x2 * x2 + y2 * y2);
+  return Math.acos(dotProduct / (magnitudeA * magnitudeB));
 }
 
 /**
